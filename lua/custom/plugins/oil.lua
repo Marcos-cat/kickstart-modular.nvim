23049b4c1aa33bb234d42dc39e9eced3f2b12db4
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2026-07-29 07:29:31 -0400

    add oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,16 @@
+vim.pack.add { 'https://github.com/stevearc/oil.nvim' }
+
+require('oil').setup {
+  keymaps = {
+    ['<leader>p'] = 'actions.preview',
+    ['<leader>.'] = 'actions.toggle_hidden',
+    ['q'] = 'actions.close',
+  },
+
+  float = {
+    max_width = 0.7,
+    max_height = 0.8,
+  },
+}
+
+vim.keymap.set('n', '-', function() require('oil').open_float() end, { desc = 'Open Oil Fileviewer' })
